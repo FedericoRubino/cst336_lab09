a30fb678bd340695ea1b63dc5fa88318d6b40183
EC2 Default User <ec2-user@ip-172-31-33-114.us-east-2.compute.internal>
2020-04-17 16:17:55 +0000

    set up done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 */
 
 var express = require("express");
-var bodyParser = require("body-parser");
+// var bodyParser = require("body-parser");
 var mysql = require("mysql");
 var app = express();
 
@@ -13,22 +13,17 @@ var app = express();
 
 const connection = mysql.createConnection({
 	host: "localhost",
-	user: "root",
-	password: "",
-	database: "quotes_db"
+	user: "frubino",
+	password: "frubino",
+	database: "new_database"
 });
+
 connection.connect();
 
-
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
-
-
 app.use(express.static("css"));
-
-app.use(bodyParser.urlencoded({extended:true}));
-
+// app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.get("/", function(req, res){
@@ -36,17 +31,38 @@ app.get("/", function(req, res){
     
 });
 
-app.get("/result", function(req, res){
-	res.render("result");
-});
-
 app.get("/author", function(req, res){
-	res.render("author");
+	var firstName = req.query.firstname;
+	var lastName = req.query.lastname;
+	var statement = 'select * ' + 
+					'from l9_author ' + 
+					'where firstname=\'' + firstName + '\'' + 
+					'and lastName=\'' + lastName + '\';';
+	connection.query(statement,function(error,found){
+		var author = null;
+		if(error) throw error;
+		if(found.length){
+			author = found[0]; // this gets us all of the data from the database of the given author
+			author.dob = author.dob.toString().split(" ").splice(0,4).join(" ");
+			author.dod = author.dod.toString().split(" ").splice(0,4).join(" ");
+		}
+		res.render('author', {author:author});
+	});
 });
 
 
 app.get("/author/:authorId", function(req, res){
-	res.render("quotes");
+	var statement = 'select firstName, lastName, quote ' + 
+					'from l9_author natural join l9_quotes ' +
+					'where l9_author.authorId=' + req.params.authorId + ';';
+	connection.query(statement,function(error,found){
+		if(error) throw error;
+		if(found.length){
+			var name = found[0].firstName + ' ' + found[0].lastName;
+			res.render('quotes', {name: name, quotes:found});
+			
+		}
+	});
 });
 
 app.get("/*", function(req, res){
