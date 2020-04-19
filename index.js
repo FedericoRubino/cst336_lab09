7ff83dea2bf78182b717943cd747d1de2bce80df
FedericoRubino <frubino@csumb.edu>
2020-04-19 18:05:27 +0000

    some finishing touches

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 /*
-
-
+Author: Federico Rubino
+email: frubino@csumb.edu
+Lab09
 */
 
 var express = require("express");
-// var bodyParser = require("body-parser");
 var mysql = require("mysql");
 var app = express();
 
-
 /* Configure mysql dbms */
-
 const connection = mysql.createConnection({
 	host: "localhost",
 	user: "frubino",
@@ -23,12 +21,9 @@ connection.connect();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.static("css"));
-// app.use(bodyParser.urlencoded({extended:true}));
-
 
 app.get("/", function(req, res){
 	var stmtCategory = "select distinct(l9_quotes.category) from l9_quotes;";
-	// var stmtNames = "select firstname, lastName from l9_author"			
 	var stmtNames = "select * from l9_author"
 	
 	var fullnames = [];
@@ -37,43 +32,28 @@ app.get("/", function(req, res){
     	if(error) throw error;
 		if(found.length){
 			found.forEach(function(f){
-				console.log(f.firstName + " " + f.lastName);
-				
 				fullnames.push(f.firstName + " " + f.lastName);
-
 			})
-			// console.log(found);
 			authors = found;			
-			
 		}
     });
     
 	var categories = [];
 	connection.query(stmtCategory,function(error,found){
-	
     	if(error) throw error;
 		if(found.length){
 			found.forEach(function(f){
-				console.log(f.category);
 				categories.push(f.category);
 			})
-			
-			
 		}
 	    res.render('home', {categories:categories,fullnames:fullnames,authors:authors});
-	
     });
 });
 
 app.get("/quoteAuthor", function(req, res) {
     var name = req.query.name;
-    console.log(name);
     var fname = name.split(" ").splice(0,1);
     var lname = name.split(" ").splice(1,2);
-    console.log(fname, lname);
-
-    
-    
     var statement = "select * from l9_quotes natural join l9_author "+
     				"where l9_author.firstName = '" + fname + "'" +
     				"and l9_author.lastName='"+ lname + "';" ; 
@@ -84,13 +64,11 @@ app.get("/quoteAuthor", function(req, res) {
 			authors = found; // this gets us all of the data from the database of the given author
 		}
 		res.render('quotes', {keyword:name, authors:authors});
-    	
     });
 });
 
 app.get("/quoteCategory", function(req, res) {
     var categ = req.query.category;
-    
     var statement = "select * from l9_quotes natural join l9_author "+
     				"where l9_quotes.category = '" + categ + "';" ; 
     connection.query(statement,function(error,found){
@@ -100,46 +78,21 @@ app.get("/quoteCategory", function(req, res) {
 			authors = found; // this gets us all of the data from the database of the given author
 		}
 		res.render('quotes', {keyword:categ, authors:authors});
-    	
     });
 });
 
 app.get("/quoteKeyword", function(req, res) {
     var keyword = req.query.keyword;
-    
-    
     var statement = "select * from l9_quotes natural join l9_author where l9_quotes.quote like '%" +keyword+ "%'" ; //TODO: more here
     connection.query(statement,function(error,found){
     	var authors = null;
     	if(error) throw error;
 		if(found.length){
-			console.log(found);
 			authors = found; // this gets us all of the data from the database of the given author
 		}
 		res.render('quotes', {keyword:keyword, authors:authors});
-    	
     });
 });
-
-// app.get("/author", function(req, res){
-// 	var firstName = req.query.firstname;
-// 	var lastName = req.query.lastname;
-// 	var statement = 'select * ' + 
-// 					'from l9_author left outer join l9_quotes on ' +
-// 					'l9_author.authorId = l9_quotes.authorId ' +
-// 					'where l9_author.firstname=\'' + firstName + '\' ' + 
-// 					'and l9_author.lastName=\'' + lastName + '\';';
-// 	console.log(statement);
-// 	connection.query(statement,function(error,found){
-// 		var authors = null;
-// 		if(error) throw error;
-// 		if(found.length){
-// 			authors = found; // this gets us all of the data from the database of the given author
-// 		}
-// 		res.render('quotes', {keyword:firstName,authors:authors});
-// 	});
-// });
-
 
 app.get("/gender",function(req, res){
 	var gender = req.query.gender;
@@ -150,7 +103,6 @@ app.get("/gender",function(req, res){
     	var authors = null;
     	if(error) throw error;
 		if(found.length){
-			console.log(found);
 			authors = found; // this gets us all of the data from the database of the given author
 		}
 		if(gender == "F"){
@@ -161,7 +113,6 @@ app.get("/gender",function(req, res){
 		res.render('quotes', {keyword:gender, authors:authors});     
     });
 });
-
 
 app.get("/author/:authorId", function(req, res){
 	var authorId = req.params.authorId;
